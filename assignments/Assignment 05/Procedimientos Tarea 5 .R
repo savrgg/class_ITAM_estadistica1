1f6e8574c22b2e84036cebab80e8bbd2f799ac88
Cinthya Andrade <cinthyaandrade@MacBook-Air-de-Cinthya.local>
2022-08-03 22:00:03 -0500

    Algunos calculos de las primeras preguntas de la tarea 5

---
+++ assignments/Assignment 05/Procedimientos Tarea 5 .R
@@ -0,0 +1,21 @@
+
+
+
+pnorm(34,50,50,lower.tail = TRUE)
+
+
+pnorm(59,50,50,lower.tail = TRUE) - pnorm(40,50,50,lower.tail = TRUE)
+
+1- pnorm(74,50,50,lower.tail = TRUE)
+pnorm(74,50,50,lower.tail = TRUE)
++7^1/2
+
+
+pnorm(4,7,3.5,lower.tail = TRUE,)
+
+
+pnorm(8,7,3.5,lower.tail = TRUE,) - pnorm(3,7,3.5,lower.tail = TRUE,)
+
+pnorm(7,7,3.5,lower.tail = TRUE) 
+1- pnorm(2,7,3.5,lower.tail = FALSE)
+0.5 - .07656373
